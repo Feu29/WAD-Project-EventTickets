c8d658dd63ed2b246261b7cdc61ebc3e7d928c1f
Feu29 <alfeusfeu@gmail.com>
2025-10-19 18:52:18 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sign Up | University Events</title>
-  <link rel="stylesheet" href="login.css">
+  <link rel="stylesheet" href="style1.css">
 </head>
 <body>
   <div class="login-container">
